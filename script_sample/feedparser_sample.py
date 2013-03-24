a88c006f445ccd340acb7b8ff905199703888354
iwry <ryo.iwa.0813@gmail.com>
2013-03-24 15:12:23 +0900

    get rss feed on console
    rssのエントリをコンソール上に表示。とりあえず次にファイル書き出しをする

---
+++ script_sample/feedparser_sample.py
@@ -0,0 +1,17 @@
+#coding:utf-8
+
+import feedparser
+import time
+
+feeds = feedparser.parse('http://a.hatena.ne.jp/sample/rss')
+
+#Print site name
+print feeds.feed.title
+
+#Print feed's title & URL
+for i in range(10):
+	print feeds.entries[i].title
+	print feeds.entries[i].link
+	#Time of Newest article 
+	updatetime = time.strftime('%Y/%m/%d %X', feeds.entries[i].updated_parsed)
+	print updatetime
