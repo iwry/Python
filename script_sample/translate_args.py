70ca385b8c3dece149626ba5ac87fb12cbe5514d
iwry <ryo.iwa.0813@gmail.com>
2013-03-25 16:08:48 +0900

    書き直し

--- script_sample/translate_args.py
+++ script_sample/translate_args.py
@@ -7,22 +7,20 @@ import feedparser
 URL = u"http://pipes.yahoo.com/poolmmjp/ej_translation_api?_render=rss&text=%s"
 
 def main(s):
-	eng_trans_quote = quote(s)
-	api_url = URL % (eng_trans_quote, )
-	d = feedparser.parse(api_url)
-	items = d['items']
-	if len(items) == 1:
-		title = items[0].title
-		desc = items[0].description
-		return title + ":" + desc
-	return ""
+    eng_trans_quote = quote(s)
+    api_url = URL % (eng_trans_quote,)
+    d = feedparser.parse(api_url)
+    items = d['items']
+    if len(items) == 1:
+        title = items[0].title
+        desc = items[0].description
+        return title + ':' + desc
+    return ""
 
 if __name__ == "__main__":
-	import sys
-	args = sys.argv
-	if len(args) > 1:
-		s = u"".join(args[1:])
-		print s
-		r = main(s)
-		print r
-		
+    import sys
+    args = sys.argv
+    if len(args) > 1:
+        s = u" ".join(args[1:])
+        r = main(s)
+        print r
